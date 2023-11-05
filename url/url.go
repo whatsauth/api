f20c7bc6be52875c75dc837d5f8871a341148476
Rolly Maulana Awangga <awangga@gmail.com>
2023-11-05 09:09:13 +0700

    url text

--- url/url.go
+++ url/url.go
@@ -10,7 +10,7 @@ import (
 func Web(page *fiber.App) {
 	//page.Get("/", controller.Homepage)
 	page.Get("/api/device/+", controller.Device)
-	page.Post("/api/send/message", controller.SendTextMessage)
+	page.Post("/api/send/message/text", controller.SendTextMessage)
 
 	page.Post("/api/whatsauth/request", controller.PostWhatsAuthRequest)
 	page.Get("/ws/whatsauth/public", websocket.New(controller.WsWhatsAuth))
