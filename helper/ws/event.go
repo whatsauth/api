e5d63f9d74d49fded651d4d29e9148ecf9fde948
Rolly Maulana Awangga <awangga@gmail.com>
2023-11-05 20:40:04 +0700

    di loop dulu

--- helper/ws/event.go
+++ helper/ws/event.go
@@ -2,11 +2,13 @@ package ws
 
 import (
 	"log"
+	"time"
 
 	"github.com/whatsauth/watoken"
 )
 
 func MagicLinkEvent(roomId string, PublicKey, PrivateKey string) {
+	log.Print("Masuk Ke Magic Link Event, status socket : ")
 	if roomId[0:1] == "v" {
 		phonenumber := watoken.DecodeGetId(PublicKey, roomId)
 		if phonenumber != "" {
@@ -15,9 +17,14 @@ func MagicLinkEvent(roomId string, PublicKey, PrivateKey string) {
 			infologin.Uuid = roomId
 			infologin.Login = newlogin
 			infologin.Phone = phonenumber
-			res := SendStructTo(roomId, infologin)
-			log.Print("Masuk Ke Magic Link Event, status socket : ")
-			log.Print(res)
+			time.Sleep(1 * time.Second)
+			success := SendStructTo(roomId, infologin)
+			log.Print(success)
+			for !success {
+				success = SendStructTo(roomId, infologin)
+				time.Sleep(1 * time.Second)
+				log.Print(success)
+			}
 		}
 	}
 }
