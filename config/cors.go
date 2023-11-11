4afb8901b9b83aeff3650a70816d84d715ba9029
Rolly Maulana Awangga <awangga@gmail.com>
2023-11-12 05:54:54 +0700

    pake allow origin

--- config/cors.go
+++ config/cors.go
@@ -1,17 +1,21 @@
 package config
 
 import (
+	"strings"
+
 	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
-/* var origins = []string{
+var origins = []string{
 	"https://wa.my.id",
 	"https://whatsauth.my.id",
 	"https://www.whatsauth.my.id",
-} */
+	"https://my.wa.my.id",
+	"https://lapor.acad-csirt.org/",
+}
 
 var Cors = cors.Config{
-	AllowOrigins:     "*", //strings.Join(origins[:], ","),
+	AllowOrigins:     strings.Join(origins[:], ","),
 	AllowHeaders:     "Origin, Token, Content-Type",
 	ExposeHeaders:    "Content-Length",
 	AllowCredentials: true,
