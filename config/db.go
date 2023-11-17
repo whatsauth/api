c932fe1ea961a48e5a829d54c4dcb45fcfc68c00
Rolly Maulana  Awangga <rolly@awang.ga>
2023-11-17 17:31:40 +0700

    pindah db

--- config/db.go
+++ config/db.go
@@ -15,9 +15,11 @@ var DBUlbimongoinfo = atdb.DBInfo{
 var Mongoconn = atdb.MongoConnect(DBUlbimongoinfo)
 
 // elephantsql 20mb
-var Postgrestring = "postgres://obruyswq:ZPHsdZ9LYSujKDoHEIehA5uJ3LYkDbv0@satao.db.elephantsql.com/obruyswq"
+//var Postgrestring = "postgres://obruyswq:ZPHsdZ9LYSujKDoHEIehA5uJ3LYkDbv0@satao.db.elephantsql.com/obruyswq"
 
 // neon tect 100hours
-//var Postgrestring = "postgresql://awangga:z9iNkyTFZOt5@ep-steep-pine-25929021-pooler.ap-southeast-1.aws.neon.tech/whatsauth?sslmode=require"
+// var Postgrestring = "postgresql://awangga:z9iNkyTFZOt5@ep-steep-pine-25929021-pooler.ap-southeast-1.aws.neon.tech/whatsauth?sslmode=require"
+// fly.io
+var Postgrestring = "postgres://postgres:iMTFz957Ov9eTmh@127.0.0.1/whatsauth?sslmode=disable"
 
 var ContainerDB, _ = wa.CreateContainerDB(Postgrestring)
--- main_test.go
+++ main_test.go
@@ -2,69 +2,23 @@ package main
 
 import (
 	"api/config"
-	"fmt"
+	"database/sql"
 	"testing"
 
-	"github.com/stretchr/testify/require"
-	"github.com/whatsauth/watoken"
+	"github.com/lib/pq"
 )
 
-func TestWatoken(t *testing.T) {
-	//privateKey, publicKey := watoken.GenerateKey()
-	//fmt.Println("privateKey : ", privateKey)
-	//fmt.Println("publicKey : ", publicKey)
+func TestDB(t *testing.T) {
 
-	tokenstring := "v4.public.eyJleHAiOiIyMDIzLTEyLTEwVDIxOjI0OjEwKzA3OjAwIiwiaWF0IjoiMjAyMy0xMS0xMFQyMToyNDoxMCswNzowMCIsImlkIjoiNjI4ODEwODI3MjYzNzkiLCJuYmYiOiIyMDIzLTExLTEwVDIxOjI0OjEwKzA3OjAwIn3810IYPVFjPsoGFa2XVkzqcEyVoLmeZKp0UvwjWdmsKDFPaERJY2OzhMl6QCU33HMUOPjHBbKwDIOQmZN4hFoP"
-	body, err := watoken.Decode(config.PublicKey, tokenstring)
-	fmt.Println("signed : ", tokenstring)
-	fmt.Println("isi : ", body.Id)
-	require.NoError(t, err)
-	dvcs := config.ContainerDB.NewDevice()
-	fmt.Println(dvcs)
-	//waclient, err := wa.GetWaClient(body.Id, config.Client, config.Mongoconn, config.ContainerDB)
-	//qr := make(chan wa.QRStatus)
-	//wa.PairConnect(waclient, qr)
-	//resp := <-qr
-	//fmt.Println(resp)
-}
-
-/*
-var phonenumber = "6287752000300"
-var deviceid uint16
-var deviceStore *store.Device
-
-func TestGetAllDevice(t *testing.T) {
-	deviceStores, err := config.ContainerDB.GetAllDevices()
-	var deviceStore *store.Device
-	for _, dv := range deviceStores {
-		if dv.ID.User == phonenumber {
-			deviceStore = dv
-			deviceid = deviceStore.ID.Device
-			fmt.Println("device id:", deviceStore.ID.Device)
-		}
+	pgUrl, err := pq.ParseURL(config.Postgrestring)
+	db, err := sql.Open("postgres", pgUrl)
+	if err != nil {
+		panic(err)
 	}
-	//deviceStore, err := config.ContainerDB.GetDevice(types.JID{User: "6287752000300", Server: "s.whatsapp.net"})
-	fmt.Println(deviceStore)
-	log.Println(err)
-	//clientLog := waLog.Stdout("Client", "DEBUG", true)
-	//client := whatsmeow.NewClient(deviceStore, waLog.Stdout("Client", "ERROR", true))
+	defer db.Close()
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
 }
-func TestGetDevice(t *testing.T) {
-	deviceStore, err := config.ContainerDB.GetDevice(types.JID{User: phonenumber, Device: deviceid, Server: "s.whatsapp.net"})
-	fmt.Println(deviceStore)
-	log.Println(err)
-	//clientLog := waLog.Stdout("Client", "DEBUG", true)
-	//client := whatsmeow.NewClient(deviceStore, waLog.Stdout("Client", "ERROR", true))
-} */
-
-/* func TestNewClient(t *testing.T) {
-	WAClient := whatsmeow.NewClient(deviceStore, waLog.Stdout("Client", "ERROR", true))
-	var txt wa.TextMessage
-	txt.IsGroup = false
-	txt.Messages = "main test func gol"
-	txt.To = "6281312000300"
-	resp, err := wa.SendTextMessage(txt, WAClient)
-	log.Println(resp)
-	log.Println(err)
-
-} */
