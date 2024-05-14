323be75777327806d3b064fbe32c5f007177ce51
Rolly Maulana Awangga <awangga@gmail.com>
2024-05-14 09:04:20 +0700

    MOVE TO ENV

--- config/db.go
+++ config/db.go
@@ -3,12 +3,13 @@ package config
 import (
 	"database/sql"
 	"log"
+	"os"
 
 	"github.com/aiteung/atdb"
 	"go.mau.fi/whatsmeow/store/sqlstore"
 )
 
-var MongoString string = "mongodb+srv://awangga:8uPiRHynbtRuHv6X@potp.x8hnwy3.mongodb.net/waapi"
+var MongoString string = os.Getenv("MONGOSTRING")
 
 var DBUlbimongoinfo = atdb.DBInfo{
 	DBString: MongoString,
@@ -17,14 +18,7 @@ var DBUlbimongoinfo = atdb.DBInfo{
 
 var Mongoconn = atdb.MongoConnect(DBUlbimongoinfo)
 
-// elephantsql 20mb
-//var Postgrestring = "postgres://obruyswq:ZPHsdZ9LYSujKDoHEIehA5uJ3LYkDbv0@satao.db.elephantsql.com/obruyswq"
-
-// neon tect 100hours
-// var Postgrestring = "postgresql://awangga:z9iNkyTFZOt5@ep-steep-pine-25929021-pooler.ap-southeast-1.aws.neon.tech/whatsauth?sslmode=require"
-// fly.io
-// var Postgrestring = "postgres://postgres:iMTFz957Ov9eTmh@127.0.0.1/whatsauth?sslmode=disable"
-var Postgrestring = "postgres://postgres:iMTFz957Ov9eTmh@whatsauth.flycast/whatsauth?sslmode=disable"
+var Postgrestring = os.Getenv("PGSTRING")
 
 var ContainerDB *sqlstore.Container
 
