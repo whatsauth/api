556529969ffe179417d517195c04784a82fa53a2
Rolly Maulana Awangga <awangga@gmail.com>
2024-08-13 03:17:45 +0700

    untuk mobile naikin ke 18 jam

--- helper/ws/event.go
+++ helper/ws/event.go
@@ -10,7 +10,7 @@ func MagicLinkEvent(roomId string, PublicKey, PrivateKey string) {
 	if roomId[0:1] == "v" {
 		payload, err := watoken.Decode(PublicKey, roomId)
 		if err == nil {
-			newlogin, _ := watoken.Encode(payload.Id, payload.Alias, PrivateKey)
+			newlogin, _ := watoken.EncodeforHours(payload.Id, payload.Alias, PrivateKey, 18)
 			var infologin LoginInfo
 			infologin.Uuid = roomId
 			infologin.Login = newlogin
