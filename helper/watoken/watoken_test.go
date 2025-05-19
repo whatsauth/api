05f708393d7ca46360fc34423944ce7642a4a2dd
Rolly Maulana Awangga <awangga@gmail.com>
2025-05-19 17:21:24 +0700

    test enc

--- helper/watoken/watoken_test.go
+++ helper/watoken/watoken_test.go
@@ -1,14 +1,17 @@
 package watoken
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestUpdateSenderRecoiver(t *testing.T) {
-	privkey := ""
+	privkey := os.Getenv("PRIVATEKEY")
 	res, _ := EncodeforHours("628888", "123123", privkey, 43800)
 	println(res)
 	println("=====================")
 
-	pyl, _ := Decode("", res)
-	println(pyl.Id)
+	//pyl, _ := Decode("", res)
+	//println(pyl.Id)
 
 }
